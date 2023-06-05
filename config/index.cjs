a83ea06900605c350664dff70643ae930d127e92
kaisa3 <117581560+kaisa3@users.noreply.github.com>
2023-06-05 16:10:53 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -21,7 +21,7 @@ const USER_CONFIG = {
       // 想要发送的人的名字
       name: '婧婧',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: '74fd60aef69b388a054db031213d2031',
+      id: 'oivxj6uFrGUqk3pdph59MqXCDA7M',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
       useTemplateId: 'lDI4CSjFQm5fq26Ap2hXcC0t-QmhqFC7v4Vx985YzOg',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
@@ -51,7 +51,7 @@ const USER_CONFIG = {
 
 
   // 【推送完成提醒】模板id, 用来看自己有没有发送成功的那个模板
-  CALLBACK_TEMPLATE_ID: 'lDI4CSjFQm5fq26Ap2hXcC0t-QmhqFC7v4Vx985YzOg',
+  CALLBACK_TEMPLATE_ID: 'oivxj6uFrGUqk3pdph59MqXCDA7M',
 
   CALLBACK_USERS: [
     {
